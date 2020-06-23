de90c0ccf89e019dd9550ef1e620dea9d75895fa
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-06-24 00:47:30 +0530

    Add files via upload

---
+++ JavaApplication6.java
@@ -0,0 +1,25 @@
+package javaapplication2;
+
+import java.util.Scanner;
+
+public class JavaApplication6 {
+
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int[] arr = new int[5];
+        int a, b, c;
+        System.out.println("Enter 2 numbers");
+        a = ob.nextInt();
+        b = ob.nextInt();
+        try {
+            arr[10] = 100;
+            c = a / b;
+            System.out.println(c);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Hello");
+    }
+
+}
