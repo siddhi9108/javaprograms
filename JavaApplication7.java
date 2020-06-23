2ed37b883302a92f61df260330edd9e3aafe80bd
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-06-24 00:52:32 +0530

    Add files via upload

---
+++ JavaApplication7.java
@@ -0,0 +1,31 @@
+package javaapplication2;
+
+import java.util.Scanner;
+
+public class JavaApplication7 {
+
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+        int[] arr = new int[5];
+        int a, b, c;
+        System.out.println("Enter 2 numbers");
+        a = ob.nextInt();
+        b = ob.nextInt();
+        try {
+            arr[10] = 100;
+            c = a / b;
+            System.out.println(c);
+
+        } catch (ArithmeticException | ArrayIndexOutofBoundsException e) {
+            if (e instanceof ArithmeticException) {
+                System.out.println("Cannot divide by zero");
+            }
+            if (e instanceof ArrayIndexOutofBoundsException) {
+                System.out.println("Index Invalid");
+            }
+
+        }
+        System.out.println("Hello");
+    }
+
+}
