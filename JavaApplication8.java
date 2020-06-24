6cdbb965ac3d224c2f86df24b86b7c6dec029a1f
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-06-24 22:25:44 +0530

    Add files via upload

---
+++ JavaApplication8.java
@@ -0,0 +1,17 @@
+package javaapplication2;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class JavaApplication8 {
+
+    public static void main(String[] args) {
+      FileInputStream fin;
+      try{
+      }catch (Exception e){
+          
+      }
+    }
+    }
+
+
