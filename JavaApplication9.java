35b1a842185f2d96b8f4dfaec9a61e5a33eb66a3
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-06-24 22:50:49 +0530

    Add files via upload

---
+++ JavaApplication9.java
@@ -0,0 +1,19 @@
+package javaapplication2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class JavaApplication9 {
+
+    static void show() throws FileNotFoundException, ArithmeticException {
+        FileInputStream fin;
+        int a = 10, b = 0, c;
+        fin = new FileInputStream("D:\\My Document\\Desktop\\Siddhi\\My work");
+        c = a / b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
