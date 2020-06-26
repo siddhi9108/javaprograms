df4f82a0ac23fb283be6de07c06a9246e898b706
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-06-26 22:24:18 +0530

    Add files via upload

---
+++ Concating.java
@@ -0,0 +1,16 @@
+package javaapplication2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class Concating {
+
+ public static void main(String[] args) {
+		String s2="hello";
+		String s3="world";
+		System.out.println(s2.concat(s3));
+		System.out.println(s2+s3);
+ }
+}
