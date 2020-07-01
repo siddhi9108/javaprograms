d8120f6e53f2302052e6fd0e976b5dadec37c4ca
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-07-01 17:12:52 +0530

    Add files via upload

---
+++ Integer_Type.java
@@ -0,0 +1,13 @@
+package javaapplication2;
+
+import java.io.*;
+import java.util.Date;
+
+public class Integer_Type {
+
+    public static void main(String[] args) {
+        byte x = (byte) 128;
+        System.out.println(x);
+
+    }
+}
