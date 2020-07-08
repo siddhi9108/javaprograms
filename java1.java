2d1c89d29bdb47011c4b3f32cbea0eeb194ec70a
siddhi9108 <66294249+siddhi9108@users.noreply.github.com>
2020-07-08 16:36:18 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,11 @@
+class java1{
+          public static void main (String[] arr){
+                    if(arr.length==2)
+                     {
+System.out.println(arr[0]);
+System.out.println(arr[1]);
+}else{
+         System.out.println("Please enter 2 args");
+}
+}
+}
